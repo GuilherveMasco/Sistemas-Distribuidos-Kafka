1b9b12d200055608b7708d4979c370b89bc82a63
Thais Zorawski <thaiszorawski@gmail.com>
2021-12-11 08:21:20 -0300

    Clean sensitive data

--- collector.py
+++ collector.py
@@ -2,10 +2,10 @@ from tweepy import OAuthHandler
 from tweepy import Stream
 from kafka import KafkaProducer
 
-access_token = "2959043404-U8CpMLNo0AeKSRuh7U6j1Z4EIdF3j1sbOhsNNcX"
-access_token_secret =  "28KrXujhu4bhBWr8AswbkrpRctQBufpwQGSbGDo6r5Pdg"
-consumer_key =  "kEJJ0fmzlRojbxhiIaUAx7yf7"
-consumer_secret =  "CubNyDlkN2tfaeUGU84iw3vXbYuIq4HuzQAhpUkxYIgijue966"
+access_token = ""
+access_token_secret =  ""
+consumer_key =  ""
+consumer_secret =  ""
 
 class StdOutListener(Stream):
     def on_data(self, data):
