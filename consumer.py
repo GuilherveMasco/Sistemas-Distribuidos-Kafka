510fa34c6d0ce3b465a68e6417d098389af26175
Thais Zorawski <thaiszorawski@gmail.com>
2021-12-12 17:37:54 -0300

    feat (consumer): creates consumer script

---
+++ consumer.py
@@ -0,0 +1,8 @@
+from kafka import KafkaConsumer
+
+consumer = KafkaConsumer("navidad", bootstrap_servers=["localhost:9092"], auto_offset_reset="earliest", enable_auto_commit=True, group_id="my-group")
+
+counter = 1
+for message in consumer:
+	print(str(counter) + message.value.decode("utf-8") + "\n")
+	counter = counter + 1
